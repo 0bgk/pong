ede4393d7c9e5043aecab6ca4b7ff20b163694d9
riukken <hector@dallonses.com>
2024-11-10 21:55:52 +0100

    How could I think that he hated math and physics?

--- src/main.c
+++ src/main.c
@@ -1,22 +1,150 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <math.h>
+
+// CONSTANTS
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 450;
+const float PLAYER_VELOCITY = 300.f;
+
+struct Vector
+{
+  Vector2 position;
+  Vector2 size;
+  Color color;
+};
+
+struct Vector CreatePlayer(int SCREEN_WIDTH, int SCREEN_HEIGHT, Color color, short number)
+{
+  struct Vector player;
+  player.size = (Vector2){12.f, 60.f};
+  player.position.x = number == 2 ? SCREEN_WIDTH - player.size.x : 0;
+  player.position.y = (SCREEN_HEIGHT / 2) - (player.size.y / 2);
+  player.color = color;
+
+  return player;
+}
+
+struct Vector CreateBall()
+{
+  struct Vector ball;
+  ball.size = (Vector2){12.f, 12.f};
+  ball.position.y = (float)(SCREEN_HEIGHT / 2) - (ball.size.y / 2);
+  ball.position.x = (float)SCREEN_WIDTH / 2 - (ball.size.x / 2);
+  ball.color = BLACK;
+
+  return ball;
+}
+
+void DrawVector(struct Vector player)
+{
+  DrawRectangleV(player.position, player.size, player.color);
+}
+
+float CalcMRU(float v, float t)
+{
+  return v * t;
+}
+
+float CalcMRUA(float v, float t, float a)
+{
+  return v * t + 1 / 2 * a * pow(t, 2);
+}
+
+void PayerMotion(struct Vector *player, int upKey, int downKey, float deltaTime)
+{
+  if (IsKeyDown(upKey) && player->position.y > 0)
+    player->position.y -= CalcMRU(PLAYER_VELOCITY, deltaTime);
+  if (IsKeyDown(downKey) && (player->position.y + player->size.y) < SCREEN_HEIGHT)
+    player->position.y += CalcMRU(PLAYER_VELOCITY, deltaTime);
+}
 
 // Program main entry point
 int main(void)
 {
-  // Initialization
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+  // Create Vectors
+  struct Vector ball = CreateBall();
+  struct Vector berserk = CreatePlayer(SCREEN_WIDTH, SCREEN_HEIGHT, BLACK, 1);
+  struct Vector griffith = CreatePlayer(SCREEN_WIDTH, SCREEN_HEIGHT, BLACK, 2);
 
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window");
+  SetTargetFPS(120); // Set our game to run at 60 frames-per-second
+
+  float velocity = 300.f;
+  float degrees = 0.f;
+  float acceleration = 0.f;
+  // bool randomBool = (int)(GetTime() * 1000) % 2;
+  bool rigth = false;
+  bool top = true;
 
   // Main game loop
-  while (!WindowShouldClose()) // Detect window close button or ESC key
+  // Detect window close button or ESC key
+  while (!WindowShouldClose())
   {
+    // Calc delta time
+    float deltaTime = GetFrameTime();
+
+    PayerMotion(&berserk, KEY_W, KEY_S, deltaTime);
+    PayerMotion(&griffith, KEY_UP, KEY_DOWN, deltaTime);
+
+    // Vector Ball values
+    // Pythagoras
+    double radians = degrees * M_PI / 180.0;
+    float vy = velocity * sin(radians);
+    float vx = velocity * cos(radians);
+    float x = vx * deltaTime + 1 / 2 * acceleration * pow(deltaTime, 2);
+    float y = vy * deltaTime + 1 / 2 * (-9.8 + acceleration) * pow(deltaTime, 2);
+
+    // collision with the lower and upper window hegith
+    if (ball.position.y < 0 || ball.position.y > (SCREEN_HEIGHT - ball.size.y))
+    {
+      top = !top;
+    }
+
+    // Ball motion
+    ball.position.x += rigth ? -x : x;
+    ball.position.y += top ? -y : y;
+
+    // Player collition
+    if (rigth == true)
+    {
+      // Check comparations
+      for (size_t i = 0; i < berserk.size.y; i++)
+      {
+        // ball.position collides with the paddle
+        // adjust ball.position.x comparative
+        if (((int)berserk.position.y + i) <= ball.position.y + (ball.size.y / 2) && ball.position.x <= berserk.position.x)
+        {
+          float centerPosition = (i - (berserk.size.y / 2));
+          degrees = centerPosition * 180.f / berserk.size.y;
+          top = centerPosition > 30;
+          rigth = false;
+        }
+      }
+    }
+    else
+    {
+      for (size_t i = 0; i < griffith.size.y; i++)
+      {
+        // ball.position collides with the paddle
+        // adjust ball.position.x comparative
+        if (((int)griffith.position.y + i) <= ball.position.y + (ball.size.y / 2) && ball.position.x >= griffith.position.x)
+        {
+          float centerPosition = (i - (griffith.size.y / 2));
+          degrees = centerPosition * 180.f / griffith.size.y;
+          top = centerPosition > 29;
+          rigth = true;
+        }
+      }
+    }
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawText("Move player one with arrow keys", 10, 10, 20, DARKGRAY);
+    char berserkCoords[50];
+    DrawVector(ball);
+    DrawVector(berserk);
+    DrawVector(griffith);
     EndDrawing();
   }
 
